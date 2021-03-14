bb246f4da7b9a90489409772090832c0fddc0813
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-03-14 22:28:40 +0500

    MFY

--- Code/WinPracticePoject/Form1.Designer.cs
+++ Code/WinPracticePoject/Form1.Designer.cs
@@ -31,13 +31,12 @@ namespace WinPracticePoject
         {
             this.BTN_Save = new System.Windows.Forms.Button();
             this.TXT_input = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // BTN_Save
             // 
-            this.BTN_Save.Location = new System.Drawing.Point(566, 168);
+            this.BTN_Save.Location = new System.Drawing.Point(410, 168);
             this.BTN_Save.Name = "BTN_Save";
             this.BTN_Save.Size = new System.Drawing.Size(84, 31);
             this.BTN_Save.TabIndex = 0;
@@ -50,36 +49,24 @@ namespace WinPracticePoject
             this.TXT_input.Location = new System.Drawing.Point(13, 13);
             this.TXT_input.Multiline = true;
             this.TXT_input.Name = "TXT_input";
-            this.TXT_input.Size = new System.Drawing.Size(637, 149);
+            this.TXT_input.Size = new System.Drawing.Size(481, 149);
             this.TXT_input.TabIndex = 1;
             this.TXT_input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox1
+            // listBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 232);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(548, 72);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(566, 275);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(84, 29);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(501, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 147);
+            this.listBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 403);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(662, 208);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.TXT_input);
             this.Controls.Add(this.BTN_Save);
             this.Name = "Form1";
@@ -93,8 +80,7 @@ namespace WinPracticePoject
 
         private System.Windows.Forms.Button BTN_Save;
         private System.Windows.Forms.TextBox TXT_input;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Code/WinPracticePoject/Form1.cs
+++ Code/WinPracticePoject/Form1.cs
@@ -16,6 +16,7 @@ namespace WinPracticePoject
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -26,22 +27,37 @@ namespace WinPracticePoject
         private void BTN_Save_Click(object sender, EventArgs e)
         {
             //textBox1.Text = TXT_input.Text;
-            StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file.txt",true);
+            StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file" + GUID() + ".txt",true);
             obj.WriteLine(TXT_input.Text);
             obj.Close();
         }
 
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
+   
+
+       
+        public String getData(string FileName)
         {
 
-        }
+            FileInfo obj = new FileInfo(FileName);
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            StreamReader objWrite = new StreamReader(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file.txt");
+            if (obj.Exists)
+            {
+                StreamReader objWrite = new StreamReader(FileName);
+                string FileData = objWrite.ReadToEnd();
+                objWrite.Close();
+
+                return FileData;
+            }
+
+            return "";
+            /*StreamReader objWrite = new StreamReader(FileName);
             string FileData = objWrite.ReadToEnd();
             objWrite.Close();
-            textBox1.Text = FileData;
+            TXT_input.Text = FileData;*/
+        }
+
+        public string GUID(){
+            return DateTime.Now.Ticks.ToString();
         }
     }
 }
