57b2221bbe9159d155d81644bd68ad5d2c435195
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-03-26 15:53:30 +0500

    today

--- Code/WinPracticePoject/Form1.cs
+++ Code/WinPracticePoject/Form1.cs
@@ -18,68 +18,73 @@ namespace WinPracticePoject
             InitializeComponent();
             //getFileList();
         }
+
+        private void BTNSAVE_Click(object sender, EventArgs e)
+        {
+
+        }
         /*
-        public void getFileList(){
-            DirectoryInfo di = new DirectoryInfo(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\");
-            FileInfo[] fi = di.GetFiles();//listBox1. //di.Name;
-            for(int i = 0; i<=fi.Length-1;i++){
-                listBox1.Items.Insert(i, fi[i].Name);
-            }
+public void getFileList(){
+   DirectoryInfo di = new DirectoryInfo(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\");
+   FileInfo[] fi = di.GetFiles();//listBox1. //di.Name;
+   for(int i = 0; i<=fi.Length-1;i++){
+       listBox1.Items.Insert(i, fi[i].Name);
+   }
 
-        }
-            private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+}
+   private void textBox1_TextChanged(object sender, EventArgs e)
+{
 
-        }
+}
 
-        private void BTN_Save_Click(object sender, EventArgs e)
-        {
-            //textBox1.Text = TXT_input.Text;
-            StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file" + GUID() + ".txt",true);
-            obj.WriteLine(TXT_input.Text);
-            obj.Close();
+private void BTN_Save_Click(object sender, EventArgs e)
+{
+   //textBox1.Text = TXT_input.Text;
+   StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file" + GUID() + ".txt",true);
+   obj.WriteLine(TXT_input.Text);
+   obj.Close();
 
-            listBox1.Items.Clear();
-            getFileList();
-        }
+   listBox1.Items.Clear();
+   getFileList();
+}
 
-        public void BTN_Update_Click(object sender, EventArgs e)
-        {
-            //textBox1.Text = TXT_input.Text;
-            StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file" + GUID() + ".txt", true);
-            obj.WriteLine(TXT_input.Text);
-            obj.Close();
+public void BTN_Update_Click(object sender, EventArgs e)
+{
+   //textBox1.Text = TXT_input.Text;
+   StreamWriter obj = new StreamWriter(@"C:\Users\MF\Desktop\Semester# 4 Spring 2021\Code\_TXT-Files\file" + GUID() + ".txt", true);
+   obj.WriteLine(TXT_input.Text);
+   obj.Close();
 
-            listBox1.Items.Clear();
-            getFileList();
-        }
+   listBox1.Items.Clear();
+   getFileList();
+}
 
 
-        public String getData(string FileName)
-        {
+public String getData(string FileName)
+{
 
-            FileInfo obj = new FileInfo(FileName);
+   FileInfo obj = new FileInfo(FileName);
 
-            if (obj.Exists)
-            {
-                StreamReader objWrite = new StreamReader(FileName);
-                string FileData = objWrite.ReadToEnd();
-                objWrite.Close();
+   if (obj.Exists)
+   {
+       StreamReader objWrite = new StreamReader(FileName);
+       string FileData = objWrite.ReadToEnd();
+       objWrite.Close();
 
-                return FileData;
-            }
+       return FileData;
+   }
 
-            return "";
-            StreamReader objWrite = new StreamReader(FileName);
-            string FileData = objWrite.ReadToEnd();
-            objWrite.Close();
-            TXT_input.Text = FileData;
-        }
+   return "";
+   StreamReader objWrite = new StreamReader(FileName);
+   string FileData = objWrite.ReadToEnd();
+   objWrite.Close();
+   TXT_input.Text = FileData;
+}
 
-        public string GUID(){
-            return DateTime.Now.Ticks.ToString();
-        }
-         */
+public string GUID(){
+   return DateTime.Now.Ticks.ToString();
+}
+*/
 
     }
 }
