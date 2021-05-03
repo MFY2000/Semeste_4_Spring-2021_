362fb164186c466d5884e2822f6d74ef642f8c9a
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-05-03 16:23:06 +0500

    all in one

--- 3. Database Management Systems/_Project/SQLQuery1.sql
+++ 3. Database Management Systems/_Project/SQLQuery1.sql
@@ -6,85 +6,137 @@ CREATE TABLE admin_users (
   username varchar(250) NOT NULL,
   user_id varchar(10) NOT NULL,
   user_pass varchar(250) NOT NULL,
-  user_access tinyint(1) NOT NULL DEFAULT 1
+  user_access int NOT NULL DEFAULT 1
 )
-
 CREATE TABLE aboutus_setup (
-  user_id varchar(10) NOT NULL,
-   shortdesc text NOT NULL,
+  user_id varchar(10) NOT NULL, -- FK
+  dob varchar(250) NOT NULL, -- FK+PK
+  shortdesc text NOT NULL,
   heading text NOT NULL,
   subheading text NOT NULL,
   longdesc text NOT NULL,
-  website varchar(250) NOT NULL,
-  dob varchar(250) NOT NULL
+  website varchar(250) NOT NULL
+  
 )
-
 CREATE TABLE basic_setup (
-  user_id varchar(10) NOT NULL,
-   title varchar(250) NOT NULL,
+  user_id varchar(10) NOT NULL,-- FK
+  LKey int NOT NULL,--PK = FK + PK
+  title varchar(250) NOT NULL,
   description text NOT NULL,
   keyword text NOT NULL,
-  icon varchar(250) NOT NULL
+  icon varchar(250) NOT NULL,
+  Theme varchar(10) NOT NULL -- fk
 )
-
+CREATE TABLE Theme (
+  Theme varchar(10) NOT NULL, -- pk
+  Location varchar(10) NOT NULL, 
+  Customize Bit  NOT NULL DEFAULT 0,   
+)
+CREATE TABLE Theme_Customize (
+  Theme varchar(10) NOT NULL, -- fk.pk
+  user_id varchar(10) NOT NULL,-- FK,pk
+  fcolor nvarchar(6) NOT NULL,
+  bcolor varchar(6) NOT NULL,
+  fontStyle varchar(250) NOT NULL,
+  fontSize int NOT NULL,
+)
 CREATE TABLE contact (
-  user_id varchar(10) NOT NULL,
-   cname varchar(250) NOT NULL,
+  user_id varchar(10) NOT NULL,-- FK
+  Cid int NOT NULL, --PK = FK + PK
+  cname varchar(250) NOT NULL,
   cemail varchar(250) NOT NULL,
   csubject text NOT NULL,
   cmessage text NOT NULL
 )
-
 CREATE TABLE personal_setup (
-  user_id varchar(10) NOT NULL,
-   profilepic varchar(250) NOT NULL,
+  user_id varchar(10) NOT NULL, -- fk
+  profilepic varchar(250) NOT NULL,
   name varchar(250) NOT NULL,
-  twitter varchar(200) NOT NULL,
-  facebook varchar(200) NOT NULL,
-  instagram varchar(200) NOT NULL,
-  skype varchar(200) NOT NULL,
-  linkedin varchar(200) NOT NULL,
-  github varchar(200) NOT NULL,
   homewallpaper varchar(200) NOT NULL,
   professions varchar(200) NOT NULL,
   location text NOT NULL,
   mobile varchar(200) NOT NULL,
-  emailuser_id varchar(200) NOT NULL
+  emailuser varchar(200) NOT NULL -- pk
 )
-
-
+CREATE TABLE Link_setup(
+  user_id varchar(10) NOT NULL, -- FK
+  Name varchar(200) NOT NULL, -- PK = FK + PK
+  Link varchar(200) NOT NULL,
+)
 CREATE TABLE portfolio (
-  user_id varchar(10) NOT NULL,
-   projectname varchar(250) NOT NULL,
+  user_id varchar(10) NOT NULL, -- fk,pk
+  projectname varchar(250) NOT NULL, -- pk
   projectpic varchar(250) NOT NULL,
-  projectlink text NOT NULL
+  projectlink text NOT NULL,
+  projectdesc varchar(250) NOT NULL,
 )
-
-CREATE TABLE resume (
-  user_id varchar(10) NOT NULL,
-   category varchar(250) NOT NULL,
+CREATE TABLE Experience (
+  user_id varchar(10) NOT NULL, -- fk , pk
+  category varchar(250) NOT NULL, 
   title varchar(250) NOT NULL,
-  year varchar(250) NOT NULL,
-  ogname varchar(250) NOT NULL,
+  year varchar(250) NOT NULL, -- pk
+  ogname varchar(250) NOT NULL, -- pk
   workdesc text NOT NULL
 )
-
 CREATE TABLE skills (
-  user_id varchar(10) NOT NULL,
-  skill varchar(250) NOT NULL,
+  user_id varchar(10) NOT NULL, -- fk , pk
+  skill varchar(250) NOT NULL, -- pk
   score varchar(250) NOT NULL
 )
 
+-- ADD PRIMARY KEY  
 
-CREATE TABLE userinfo (
-  user_id varchar(10) NOT NULL,
-  userq varchar(250) NOT NULL,
-  userv varchar(250) NOT NULL
-)
-
-
--- Indexes for table admin_users
---
 ALTER TABLE admin_users
   ADD PRIMARY KEY (user_id);
+ALTER TABLE aboutus_setup
+  ADD PRIMARY KEY (user_id,dob);
+ALTER TABLE basic_setup
+  ADD PRIMARY KEY (user_id,Theme); 
+ALTER TABLE Theme
+  ADD PRIMARY KEY (Theme);
+ALTER TABLE Theme_Customize
+  ADD PRIMARY KEY (user_id,Theme);
+ALTER TABLE contact
+  ADD PRIMARY KEY (user_id,Cid);
+ALTER TABLE personal_setup
+  ADD PRIMARY KEY (user_id,emailuser);
+ALTER TABLE Link_setup
+  ADD PRIMARY KEY (user_id,Name);
+ALTER TABLE portfolio
+  ADD PRIMARY KEY (user_id,projectname);
+ALTER TABLE Experience
+  ADD PRIMARY KEY (user_id,year,ogname);
+ALTER TABLE skills
+  ADD PRIMARY KEY (user_id,skill);
+  --
 
+  -- ADD Foreign Key  
+
+ALTER TABLE aboutus_setup
+   ADD CONSTRAINT admin_usersfk
+   FOREIGN KEY (user_id)
+   REFERENCES admin_users (user_id);
+
+ALTER TABLE admin_users
+  ADD PRIMARY KEY (user_id);
+ALTER TABLE aboutus_setup
+  ADD PRIMARY KEY (user_id,dob);
+ALTER TABLE basic_setup
+  ADD PRIMARY KEY (user_id,Theme); 
+ALTER TABLE Theme
+  ADD PRIMARY KEY (Theme);
+ALTER TABLE Theme_Customize
+  ADD PRIMARY KEY (user_id,Theme);
+ALTER TABLE contact
+  ADD PRIMARY KEY (user_id,Cid);
+ALTER TABLE personal_setup
+  ADD PRIMARY KEY (user_id,emailuser);
+ALTER TABLE Link_setup
+  ADD PRIMARY KEY (user_id,Name);
+ALTER TABLE portfolio
+  ADD PRIMARY KEY (user_id,projectname);
+ALTER TABLE Experience
+  ADD PRIMARY KEY (user_id,year,ogname);
+ALTER TABLE skills
+  ADD PRIMARY KEY (user_id,skill);
+  --
