967dc2e5a3ecea4de4da06e29dade772c1d237d6
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-07-19 21:23:32 +0500

    Auto commits Done: 19/07/2021 21:23:32

---
+++ 3. Database Management Systems/Lab/Project/WEBAPP_portfolioMaker/Controllers/TempController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WEBAPP_portfolioMaker.Models;
+
+namespace WEBAPP_portfolioMaker.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Temp
+        public ActionResult Index()
+        {
+            var db = new Portfolio_makerEntities();
+            var data = db.Get_WebVeiw();
+            return View(data.ToList());
+        }
+
+        // GET: Temp/Details/5
+        public ActionResult Details(string id)
+        {
+
+            var db = new Portfolio_makerEntities();
+            SqlParameter param1 = new SqlParameter("@id", id);
+
+            var data = db.Database.SqlQuery<WebView>("exec Get_WebVeiw_DetailsID @id", param1).SingleOrDefault();
+
+
+            return View(data);
+        }
+
+        // GET: Temp/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Temp/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection WebView)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Temp/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Temp/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Temp/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Temp/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
